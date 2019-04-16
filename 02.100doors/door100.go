6a27ebfbda19fa3edc95a6c3dfceed88454db961
guzhongren <guzhongren@live.cn>
2019-04-16 23:31:24 +0800

    continue(100door)： 继续

--- 02.100doors/door100.go
+++ 02.100doors/door100.go
@@ -1,15 +1,24 @@
 package main
 
-type Door100 struct {
-	Data [100]bool
+import "fmt"
+
+var Data [100]bool
+
+func Init() [100]bool {
+	for i := 0; i < 100; i++ {
+		Data[i] = false
+	}
+	return Data
 }
 
-func (door100 Door100) Run(num int) [100]bool {
-	for key, _ := range door100.Data {
-		if key%num == 0 {
-			door100.Data[key] = !door100.Data[key]
+func Run(number int) [100]bool {
+	fmt.Println(number)
+	for j := number; j > 0; j-- {
+		for i := 1; i <= 100; i++ {
+			if i%j == 0 {
+				Data[i] = !Data[i]
+			}
 		}
-
 	}
-	return door100.Data
+	return Data
 }
--- 02.100doors/door100_test.go
+++ 02.100doors/door100_test.go
@@ -4,56 +4,33 @@ import (
 	"testing"
 )
 
-func TestStruct(t *testing.T) {
-	door100 := Door100{}
-	_, ok := interface{}(door100).(Door100)
-	if !ok {
-		t.Errorf("Life is not normal , expect %v, but got %v", door100, Door100{})
-	}
-}
-
 func TestStructData(t *testing.T) {
-	door100 := Door100{}
-	got := len(door100.Data)
+	getInitData := Init()
+	got := len(getInitData)
 	if got != 100 {
 		t.Errorf("Life is not normal , expect %d, but got %d", 100, got)
+	}
+	expect := [3]bool{false, false, false}
+	result := [3]bool{getInitData[0], getInitData[1], getInitData[2]}
+	if expect != result {
+		t.Errorf("exec Init(), expect %v, but got %v", expect, got)
 	}
 }
 
 func TestRun1(t *testing.T) {
-	door100 := Door100{}
-	got := door100.Run(1)
-	var expect [100]bool
-	for key, _ := range expect {
-		expect[key] = false
-	}
-	if got != expect {
-		t.Errorf("Run 1 , expect %v, but got %v", expect, got)
+	got := Run(1)
+	actual := [3]bool{got[0], got[1], got[2]}
+	expect := [3]bool{true, true, true}
+	if expect != actual {
+		t.Errorf("exec Run(1), expect %v, but got %v", expect, got)
 	}
 }
 
 func TestRun2(t *testing.T) {
-	door100 := Door100{}
-	got := door100.Run(2)
-	var expect [100]bool
-	for key, _ := range expect {
-		expect[key] = false
-	}
-	for index, _ := range expect {
-		if index%2 == 0 {
-			expect[index] = !expect[index]
-		}
-	}
-	if got != expect {
-		t.Errorf("Run 1 , expect %v, but got %v", expect, got)
-	}
-}
-func TestRun3(t *testing.T) {
-	door100 := Door100{}
-	got := door100.Run(3)
-	var expect = [true, true, true]
-	
-	if got != expect {
-		t.Errorf("Run 1 , expect %v, but got %v", expect, got)
+	got := Run(2)
+	actual := [3]bool{got[0], got[1], got[2]}
+	expect := [3]bool{true, false, true}
+	if expect != actual {
+		t.Errorf("exec Run(2), expect %v, but got %v", expect, got)
 	}
 }
